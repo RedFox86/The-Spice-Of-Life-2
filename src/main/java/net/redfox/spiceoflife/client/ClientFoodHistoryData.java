a4f1711256e50451a9f7965b376df498811fdb1a
Peter <85247098+RedFox86@users.noreply.github.com>
2025-08-10 21:07:46 -0400

    Fixed #3

--- src/main/java/net/redfox/spiceoflife/client/ClientFoodHistoryData.java
+++ src/main/java/net/redfox/spiceoflife/client/ClientFoodHistoryData.java
@@ -3,7 +3,7 @@ package net.redfox.spiceoflife.client;
 import java.util.ArrayList;
 
 public class ClientFoodHistoryData {
-  private static ArrayList<String> playerFoodHistory;
+  private static ArrayList<String> playerFoodHistory = new ArrayList<>();
 
   public static void set(ArrayList<String> history) {
     ClientFoodHistoryData.playerFoodHistory = history;
